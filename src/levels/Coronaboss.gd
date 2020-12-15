f3f151a41df6a65d74e03f0176021b9850b3da2e
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-14 21:40:05 -0300

    corona_boss

---
+++ src/levels/Coronaboss.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var flip = true
+var posicao_inicial
+var posicao_final_direita
+var posicao_final_esquerda
+var velocidade = 10
+
+func _ready():
+	posicao_inicial = $".".position.x
+	posicao_final_direita = posicao_inicial + 720
+	posicao_final_esquerda = posicao_inicial - 720
+	
+func _process(delta):
+	if(posicao_inicial <= posicao_final_direita and flip):
+		$"." . position.x += velocidade;
+		if($".".position.x >= posicao_final_direita):
+			flip = false
+		
+	if($".".position.x >= posicao_final_esquerda and !flip):
+		$".".position.x -= velocidade
+		if($".".position.x <= posicao_final_esquerda):
+			flip = true
+	
+
