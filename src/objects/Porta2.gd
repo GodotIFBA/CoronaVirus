83a47f8164d8dcd1c62c93e52fc3982313f18cc3
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-16 13:04:32 -0300

    ajustes levels

---
+++ src/objects/Porta2.gd
@@ -0,0 +1,15 @@
+tool
+extends Area2D 
+
+onready var animation: AnimationPlayer = get_node("AnimationPlayer")
+
+export(String, FILE, "*tscn") var next_scene
+
+func _on_Porta_body_entered(body) -> void:
+	teleport()
+
+
+func teleport() -> void:
+	animation.play("fade in")
+	yield(animation, "animation_finished")
+	get_tree().change_scene("res://src/levels/Level3.tscn")
