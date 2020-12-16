25bb1d5977018724884e934f53a4936903f0af1a
amandabriena <amanda.briena@gmail.com>
2020-12-15 23:24:35 -0300

    player coletando a mascara

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,25 +1,41 @@
 extends Actor
+var mascarado = false
 
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("move_up")
 	var direction = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-
-	if !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
+	
+	if !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right") and mascarado==false:
 		$player.play("Jump")
 		$player.flip_h = false
-	elif !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
+	elif !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left") and mascarado==false:
 		$player.play("Jump")
 		$player.flip_h = true
-	elif Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right") and mascarado==false:
 		$player.play("Run")
 		$player.flip_h = false
-	elif Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left") and mascarado==false:
 		$player.play("Run")
+		$player.flip_h = true
+	elif !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right") and mascarado==true:
+		$player.play("Jump Mask")
+		$player.flip_h = false
+	elif !is_on_floor() and Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left") and mascarado==true:
+		$player.play("Jump Mask")
+		$player.flip_h = true
+	elif Input.is_action_pressed("move_right") and mascarado==true:
+		$player.play("Run Mask")
+		$player.flip_h = false
+	elif Input.is_action_pressed("move_left") and mascarado==true:
+		$player.play("Run Mask")
 		$player.flip_h = true
 	else:
-		$player.play("Idle")
+		if mascarado == false:
+			$player.play("Idle")
+		else:
+			$player.play("Idle Mask")
 	
 func get_direction() -> Vector2:
 	return Vector2(
@@ -41,3 +57,13 @@ func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed
 		out.y = 0.0
 	
 	return out
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Mask":
+		mascarado = true
+
+
+func _on_Area2D_area_entered(area):
+	if area.name == "Mask":
+		mascarado = true
