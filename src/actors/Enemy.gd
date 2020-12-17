1fd92d5f3f242891280c0b7b5825a0763c4bb0c9
amandabriena <amanda.briena@gmail.com>
2020-12-17 20:39:37 -0300

    enemy movimento

--- src/actors/Enemy.gd
+++ src/actors/Enemy.gd
@@ -2,11 +2,20 @@ extends Actor
 
 func _ready() -> void:
 	_velocity.x = -speed.x
+	$enemy.play("walk")
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	
 	if is_on_wall():
 		_velocity.x *= -1.0
-		$"enemy-idle".scale.x *= -1.0
+		$enemy.scale.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
+func _on_Area2D_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_StompDetector_area_entered(area):
+	queue_free()
