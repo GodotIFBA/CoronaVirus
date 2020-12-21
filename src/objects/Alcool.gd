635313eb48ded7379d31956c420e40723570a1c4
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-21 14:05:17 -0300

    correçao alcool

--- src/objects/Alcool.gd
+++ src/objects/Alcool.gd
@@ -3,4 +3,6 @@ extends Area2D
 onready var animation: AnimationPlayer = get_node("AnimationPlayer")
 
 func _on_Alcool_body_entered(body):
+		Global.life += 1
+		print(Global.life)
 		animation.play("fade out")
