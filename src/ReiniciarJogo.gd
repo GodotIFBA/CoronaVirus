466f910d57d4bffea6801b34ba5e64505b0e6dfe
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-21 13:42:44 -0300

    game over

--- src/ReiniciarJogo.gd
+++ src/ReiniciarJogo.gd
@@ -1,15 +1,4 @@
 extends Button
 
-export var reference_path = ""
-export(bool) var start_focused = false
-onready var audioS = $AudioStreamPlayer
-
-func _ready():
-		grab_focus()
-	
-func _on_ReiniciarJogo_mouse_entered():
-	grab_focus()
-
-
-func _on_ReiniciarJogo_pressed():
-	get_tree().change_scene("res://src/levels/Level1.tscn")
+func _on_Button_pressed():
+	get_tree().change_scene("res://src/menu/Background.tscn")
