c4edcea3bd2c3219430ad8f8bad90457b8e8dbfd
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-22 12:24:47 -0300

    ajustes e nerf coronaboss

--- src/ReiniciarJogo.gd
+++ src/ReiniciarJogo.gd
@@ -1,7 +1,7 @@
 extends Button
 
 func _on_Button_pressed():
-	Global.life = 5
+	Global.life = 6
 	Global.score = 0
 	Global.componentes = 0
 	get_tree().change_scene("res://src/menu/Background.tscn")
