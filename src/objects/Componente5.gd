b0703d6f66a76102950845aeccea57a2eb65c267
1Skyzor <emersonsantanadejesus@gmail.com>
2020-12-22 16:07:58 -0300

    logica da vacina

--- src/objects/Componente5.gd
+++ src/objects/Componente5.gd
@@ -2,6 +2,12 @@ extends Area2D
 
 onready var animation: AnimationPlayer = get_node("AnimationPlayer")
 
+func _physics_process(delta):
+	if Global.componentes == 4:
+		visible = true
 
 func _on_Componente5_body_entered(body):
+	if(visible == true):
+		
+		Global.vacina = true
 	animation.play("fade out")
